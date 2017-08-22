16ab86636249d37e6a535c61612e28c0aa28c3e5
suryamurugan <surya3542live@gmail.com>
2017-08-22 19:37:33 +0530

    Initial commit

---
+++ app/src/main/java/com/murugan/surya/abc/ItemFiveFragment.java
@@ -0,0 +1,24 @@
+package com.murugan.surya.abc;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ItemFiveFragment extends Fragment {
+    public static ItemFiveFragment newInstance() {
+        ItemFiveFragment fragment = new ItemFiveFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_item_five, container, false);
+    }
+}
---
+++ app/src/main/java/com/murugan/surya/abc/MainActivity.java
@@ -0,0 +1,118 @@
+package com.murugan.surya.abc;
+
+/*
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
+*/
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.CS:
+                                selectedFragment = ItemOneFragment.newInstance();
+                                break;
+                            case R.id.IS:
+                                selectedFragment = ItemTwoFragment.newInstance();
+                                break;
+                            case R.id.EC:
+                                selectedFragment = ItemThreeFragment.newInstance();
+                                break;
+
+                            case R.id.ME:
+                                selectedFragment = ItemFiveFragment.newInstance();
+                                break;
+
+                            case R.id.CV:
+                                selectedFragment = ItemSixFragment.newInstance();
+                                break;
+                           // case R.id.idk:
+                              //  selectedFragment = ItemFourFragment.newInstance();
+                              //  break;
+
+
+
+
+
+
+
+
+                        }
+                       FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                       transaction.replace(R.id.frame_layout, selectedFragment);
+                       transaction.commit();
+                        return true;
+                    }
+                });
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, ItemOneFragment.newInstance());
+        transaction.commit();
+
+        //Used to select an item programmatically
+        //bottomNavigationView.getMenu().getItem(2).setChecked(true);
+    }
+}
